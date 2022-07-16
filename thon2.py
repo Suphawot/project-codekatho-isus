e61f1cf89d55d7ff170309e32322e3ef5c26e44c
Suphawot <109365085+Suphawot@users.noreply.github.com>
2022-07-16 11:51:33 +0700

    Update thon2.py

--- thon2.py
+++ thon2.py
@@ -7,39 +7,43 @@ import numpy as np
 cap = VideoCapture(0)
 sensor = ultrasonic.distance_cm()
 yolo=parinya.YOLOv3(‘coco.names.txt’,’yolov3-tiny.cfg ’,’yolov3-tiny.weights’)
-Green light_Pin=Pin(14,Pin_OUT)
-Yellow light_Pin=Pin(23,Pin_OUT)
-Red light_Pin=Pin(27,Pin_OUT)
-Crossgreen light_Pin=Pin(11,Pin_OUT)
+green light_Pin=Pin(14,Pin_OUT)
+yellow light_Pin=Pin(23,Pin_OUT)
+red light_Pin=Pin(27,Pin_OUT)
+crossgreen light_Pin=Pin(11,Pin_OUT)
 crossred light_Pin=Pin(17,Pin_OUT)
-Buzzer = machine.Pin(23, machine.Pin.OUT)
-Line_detect=cv2.pointPolygonTest(np.array(area1, np.int32), (int(cx), int(cy)), false)
+p32 = machine.Pin(32, Pin_OUT)
+buzzer = machine.PWM(p32)
+line_detect=cv2.pointPolygonTest(np.array(area1, np.int32), (int(cx), int(cy)), false)
 vid = VideoWriter('cctv.avi', VideoWriter_fourcc(*'MP42'), 25.0, (640,400))
 
+buzzer.freq(98)
+height = image.shape[0]
+width = image.shape[1]
+ 
+cv2.line(cap, (500, 499), (1449,1128), (255,0,0), 2)
+ 
 While True:
     _, frame = cap.read
-    yolo.detect(frame)
     grayframe= cv2.cvtColour(frame, cv2.COLOR_BGR2GRAY)
-    Crossred light_Pin.Value(1)
+    Crossred light_Pin.value(1)
     Green light_Pin.value(1)
-    if sensor <= 10:
-        Crossred light_Pin.value(0)
-        Green light_Pin.value(0)
-        Yellow light_Pin.value(1)
-        elif line_detect <= 0:
-            Buzzer.duty(50)
-            time.sleep(1)
-            Buzzer.duty(0)
-            Crossred light_Pin.Value(1)
-            Green light_Pin.value(0)
-            Yellow light_Pin.Value(1)
-            Time.sleep(5)
+    if sensor >= 10:
+        buzzer.duty(512)
+        time.sleep(1)
+        buzzer.duty(0)
+        green light_Pin.value(0)
+        yellow light_Pin.value(1)
+        time.sleep(3)
+        red light_Pin.value(1)
+        time.sleep(5)
+        elif class_name ['car','motorbike','bus','truck'] in line_detect <= 0:
+            yolo.detect(frame)
             vid.write(frame)
+            time.sleep(10)
             elif line_detect > 0:
                 red light_Pin.value(1)
                 crossred light_Pin.value(1)
-                Elif result<0:
-                red light_Pin.value(1)
                 time.sleep(60)
                 Buzzer.duty(50)
                 Time.sleep(1)
@@ -53,7 +57,7 @@ While True:
                 cv2.imshow(‘frame’, frame)
                 if cv2.waitKey(1):
                     break   
-    else sensor==0:
+    else:
         break
         
 vid.release()  
@@ -102,12 +106,12 @@ while True:
         Green light_Pin.off()
         yellow light_Pin.on()
         time.sleep(5)
-        elif line_detect <= 10: #if it have any object accross the line will be show this loop
+        elif line_detect <= 1: #if it have any object accross the line will be show this loop
             cv2.restangle(frame, (x, y), (x2, y2), colour, 2)
             cv2.rectangle(frame, (x, y), (x + 100, y-30), colour, -1)
             red light_Pin.on()
             vidfile.write(frame)
-            elif line_detect > 10: #if it don't have any object accross the line will be show this loop
+            elif line_detect > 0: #if it don't have any object accross the line will be show this loop
                 red light_Pin.on()
                 crossgreen light.on()
                 time.sleep(10)
